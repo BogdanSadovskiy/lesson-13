53dabe6c3452e8b00a0e818e161deddaf50def1d
BogdanSadovskiy <36935732+BogdanSadovskiy@users.noreply.github.com>
2023-03-26 23:29:23 +0300

    Add files via upload

---
+++ task3 (counter of identical values).cpp
@@ -0,0 +1,45 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+	const int n = 5;
+	int arr[n][n];
+	srand(time(NULL));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			arr[i][j] = rand() % 20 + 1;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	int numb;
+	int id = 0;
+	const int m = 20;
+	int mini[m];
+	for (int k = 1; k <= 20; k++) {
+		numb = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (arr[i][j] == k) {
+					numb++;
+					
+				}
+			}
+		}
+		if (numb > 1) {
+			mini[id] = k;
+			id++;
+		}
+	}
+
+	for (int i = 0; i < id; i++) {
+			cout << mini[i]<< " ";
+		
+	}
+}
