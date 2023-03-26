6d05bec9bc8e13c645b71975017ab1d622ae4d38
BogdanSadovskiy <36935732+BogdanSadovskiy@users.noreply.github.com>
2023-03-27 02:29:02 +0300

    Add files via upload

---
+++ adding 1.cpp
@@ -0,0 +1,30 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+
+	const int n = 5;
+	const int m = 5;
+
+	int a;
+	cout << "Input first number \n";
+	cin >> a;
+	int arr[n][m];
+	arr[0][0] = a;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (i == 0 && j == 0) {
+				continue;
+			}
+			arr[i][j] = a + 1;
+			a = arr[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
