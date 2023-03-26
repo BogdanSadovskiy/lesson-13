57e8ab1f9b3b9c896f48cae9af2c58cb0b2a2d81
BogdanSadovskiy <36935732+BogdanSadovskiy@users.noreply.github.com>
2023-03-27 00:53:57 +0300

    Add files via upload

---
+++ elephant move.cpp
@@ -0,0 +1,63 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+	// хід слона
+	const int n = 8;
+	const int m = 8;
+
+	int arr[n][m];
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			arr[i][j] = 0;
+		}
+	}
+
+	int r, c;
+	cout << "Input a row\n"; cin >> r;
+	cout << "Input a columb\n"; cin >> c;
+	r--;
+	c--;
+	
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	for (int i = r, j = c; j >= 0 && i >= 0; i--, j--) {
+		if (i >= 0 && j >= 0) {
+			arr[i][j] = 2;
+		}
+	}
+
+	for (int i = r, j = c; j>=0  && i < n; i++, j--) {
+		if (j >= 0) {
+			arr[i][j] = 2;
+		}
+	}
+
+	for (int i = r, j = c; j < n && i < n; i++, j++) {
+		arr[i][j] = 2;
+	}
+	for (int i = r, j = c; j < n && i >= 0; i--, j++) {
+		if (i >= 0 && j >= 0) {
+			arr[i][j] = 2;
+		}
+	}
+	arr[r][c] = 1;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+
+}
